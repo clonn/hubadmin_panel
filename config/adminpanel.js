22bb2235662489f1520a092a8fff32cc8add6a92
alincode <alin.code@gmail.com>
2016-06-07 18:44:39 +0800

    gamelist ui init

--- config/adminpanel.js
+++ config/adminpanel.js
@@ -1,78 +1,108 @@
 'use strict';
 
 module.exports.adminpanel = {
-    instances: {
+  instances: {
 
-        users: {
+    users: {
 
-            title: 'Users',
-            model: 'User',
+      title: 'Users',
+      model: 'User',
 
-            list: {
-                fields: {
-                    id: 'ID',
-                    email: 'Email',
-                    active: 'Active',
-                    admin: 'Admin',
-                    createdAt: 'Created'
-                }
-            },
-            edit: {
-                fields: {
-                    email: 'Email',
-
-                    active: {
-                        title: 'Active'
-                    },
-                    admin: {
-                        title: 'Admin',
-                        disabled: true
-                    }
-                }
-            }
-        },
-
-        Post: {
-
-            title: 'Posts',
-            model: 'Post',
-
-            list: {
-                fields: {
-                    id: 'ID',
-                    title: 'Post Title',
-                    content: 'Post Content',
-                    auth: 'Post Author',
-                    createdAt: 'Created'
-                }
-            },
-            add: {
-                fields: {
-                    id: 'ID',
-                    title: 'Post Title',
-                    content: 'Post Content',
-                    auth: 'Post Author',
-                    // createdAt: 'Created'
-                }  
-            },
-
-            edit: {
-                fields: {
-                    title: 'Post Title',
-                    content: 'Post Content',
-                    auth: 'Post Author',
-                    // createdAt: {
-                    //     disabled: true
-                    // }
-                    // active: {
-                    //     title: 'Active'
-                    // },
-                    // admin: {
-                    //     title: 'Admin',
-                    //     disabled: true
-                    // }
-                }
-            }
+      list: {
+        fields: {
+          id: 'ID',
+          email: 'Email',
+          active: 'Active',
+          admin: 'Admin',
+          createdAt: 'Created'
         }
-    }
+      },
+      edit: {
+        fields: {
+          email: 'Email',
+
+          active: {
+            title: 'Active'
+          },
+          admin: {
+            title: 'Admin',
+            disabled: true
+          }
+        }
+      }
+    },
+
+    Post: {
+
+      title: 'Posts',
+      model: 'Post',
+
+      list: {
+        fields: {
+          id: 'ID',
+          title: 'Post Title',
+          content: 'Post Content',
+          auth: 'Post Author',
+          createdAt: 'Created'
+        }
+      },
+      add: {
+        fields: {
+          id: 'ID',
+          title: 'Post Title',
+          content: 'Post Content',
+          auth: 'Post Author',
+          // createdAt: 'Created'
+        }
+      },
+
+      edit: {
+        fields: {
+          title: 'Post Title',
+          content: 'Post Content',
+          auth: 'Post Author',
+          // createdAt: {
+          //     disabled: true
+          // }
+          // active: {
+          //     title: 'Active'
+          // },
+          // admin: {
+          //     title: 'Admin',
+          //     disabled: true
+          // }
+        }
+      }
+    },
+
+    GameList: {
+      title: 'GameLists',
+      model: 'GameList',
+      list: {
+        fields: {
+          id: 'ID',
+          title: 'GameList Title',
+          like: 'Like',
+          count: 'Count',
+          createdAt: 'Created'
+        }
+      },
+      add: {
+        fields: {
+          title: 'GameList Title',
+          like: 'Like',
+          count: 'Count',
+        }
+      },
+      edit: {
+        fields: {
+          title: 'GameList Title',
+          like: 'Like',
+          count: 'Count',
+        }
+      }
+    },
+  },
+
+
 };
